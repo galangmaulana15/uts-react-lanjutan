37b884b45854c408086f6464f655e923822cde21
galangmaulana15 <galangponco123@gmail.com>
2026-03-15 23:43:24 +0700

    Add vercel configs and env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,8 @@ import App from './App'
 import './index.css'
 
 // Konfigurasi axios
-axios.defaults.baseURL = 'http://localhost:5000'
-axios.defaults.withCredentials = true
-
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+axios.defaults.withCredentials = true;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
